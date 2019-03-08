36a2672f276081b57c8e83bc22c675ab796050d2
sitimaghfuroh <48342747+sitimaghfuroh@users.noreply.github.com>
2019-03-07 20:40:26 -0800

    Add files via upload

---
+++ sitimaghfuroh_17090103_4b.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Siti Maghfuroh
+ */
+public class sitimaghfuroh_17090103_4b {
+    public static void main(String args[]){
+        System.out.println("Nama = Siti Maghfuroh"
+                + "Nim = 17090103"
+                + "Kelas = 4B");
+    }  
+}
